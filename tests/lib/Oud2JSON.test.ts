4a5b411b57a3d53c706f738f0a3a1aeba3fc922f
up-tri <ktarou78@gmail.com>
2023-08-14 14:47:22 +0900

    深すぎるネストの場合にエラーを吐くテストケースを追加

--- tests/lib/Oud2JSON.test.ts
+++ tests/lib/Oud2JSON.test.ts
@@ -1,5 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { InvalidFileTypeError } from '../../src/errors/InvalidFileTypeError';
+import { NestingLevelTooDeepError } from '../../src/errors/NestingLevelTooDeepError';
 import { Oud2JSON } from '../../src/libs/Oud2JSON';
 
 describe('Oud2JSON', () => {
@@ -26,7 +28,17 @@ describe('Oud2JSON', () => {
       const ins = new Oud2JSON(oudFileLines);
       expect(() => {
         ins.parse();
-      }).toThrowError();
+      }).toThrowError(InvalidFileTypeError);
+    });
+    test('raise error with invalid data (2)', async () => {
+      const oudFileLines = fs
+        .readFileSync(path.join(__dirname, './resources/mock/invalid_file_pattern.oud'), 'utf8')
+        .split('\n');
+
+      const ins = new Oud2JSON(oudFileLines);
+      expect(() => {
+        ins.parse();
+      }).toThrowError(NestingLevelTooDeepError);
     });
   });
 });
