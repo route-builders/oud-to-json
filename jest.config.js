c31b5e50c697b120a86b8e965f8e0a8945c866cd
up-tri <ktarou78@gmail.com>
2023-08-14 15:23:55 +0900

    カバレッジを取る設定を追加

--- jest.config.js
+++ jest.config.js
@@ -15,4 +15,14 @@ module.exports = {
       },
     ],
   },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
