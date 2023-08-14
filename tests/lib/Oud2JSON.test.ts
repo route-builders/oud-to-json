8b1a906d4dc6d33e96e02c7f1ddd66a18cc7ed04
up-tri <ktarou78@gmail.com>
2023-08-14 14:40:22 +0900

    意味の無いコメントを削除

--- tests/lib/Oud2JSON.test.ts
+++ tests/lib/Oud2JSON.test.ts
@@ -5,7 +5,6 @@ import { Oud2JSON } from '../../src/libs/Oud2JSON';
 describe('Oud2JSON', () => {
   describe('Oud2JSON#parse', () => {
     test('parse with valid data', async () => {
-      // read sample data
       const oudFileLines = fs
         .readFileSync(path.join(__dirname, './resources/mock/ressya_patterns.oud'), 'utf8')
         .split('\n');
