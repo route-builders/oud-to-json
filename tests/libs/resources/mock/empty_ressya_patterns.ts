ab4039a7fa40626f8a2b79f31dfd54ea92104862
up-tri <ktarou78@gmail.com>
2024-09-19 01:08:41 +0900

    各要素の行番号をメタ情報として追加

--- tests/libs/resources/mock/empty_ressya_patterns.ts
+++ tests/libs/resources/mock/empty_ressya_patterns.ts
@@ -8,26 +8,46 @@ export default {
           Ekimei: 'eki_normal__starting_terminal',
           Ekijikokukeisiki: 'Jikokukeisiki_NoboriChaku',
           Ekikibo: 'Ekikibo_Ippan',
+          __meta__: {
+            entry: 3,
+            last: 7,
+          },
         },
         {
           Ekimei: 'eki_normal__dep_only',
           Ekijikokukeisiki: 'Jikokukeisiki_Hatsu',
           Ekikibo: 'Ekikibo_Ippan',
+          __meta__: {
+            entry: 8,
+            last: 12,
+          },
         },
         {
           Ekimei: 'eki_core__dep_only',
           Ekijikokukeisiki: 'Jikokukeisiki_Hatsu',
           Ekikibo: 'Ekikibo_Syuyou',
+          __meta__: {
+            entry: 13,
+            last: 17,
+          },
         },
         {
           Ekimei: 'eki_normal__dep_and_arrv',
           Ekijikokukeisiki: 'Jikokukeisiki_Hatsuchaku',
           Ekikibo: 'Ekikibo_Ippan',
+          __meta__: {
+            entry: 18,
+            last: 22,
+          },
         },
         {
           Ekimei: 'eki_normal_last_terminal',
           Ekijikokukeisiki: 'Jikokukeisiki_KudariChaku',
           Ekikibo: 'Ekikibo_Ippan',
+          __meta__: {
+            entry: 23,
+            last: 27,
+          },
         },
       ],
       Ressyasyubetsu: [
@@ -38,18 +58,30 @@ export default {
           DiagramSenColor: '00000000',
           DiagramSenStyle: 'SenStyle_Jissen',
           StopMarkDrawType: 'EStopMarkDrawType_Nothing',
+          __meta__: {
+            entry: 28,
+            last: 35,
+          },
         },
       ],
       Dia: [
         {
           DiaName: 'target_dia',
-          Kudari: { Ressya: [] },
-          Nobori: { Ressya: [] },
+          Kudari: { Ressya: [], __meta__: { entry: 38, last: 39 } },
+          Nobori: { Ressya: [], __meta__: { entry: 40, last: 41 } },
+          __meta__: {
+            entry: 36,
+            last: 42,
+          },
         },
       ],
       KitenJikoku: '400',
       DiagramDgrYZahyouKyoriDefault: '60',
       Comment: 'comment\\nafterbreakline',
+      __meta__: {
+        entry: 1,
+        last: 46,
+      },
     },
   ],
   DispProp: {
@@ -74,6 +106,14 @@ export default {
     DiaJikuColor: '00C0C0C0',
     EkimeiLength: '6',
     JikokuhyouRessyaWidth: '6',
+    __meta__: {
+      entry: 47,
+      last: 67,
+    },
   },
   FileTypeAppComment: 'OuDia Ver. 1.02.02',
+  __meta__: {
+    entry: 0,
+    last: 68,
+  },
 };
